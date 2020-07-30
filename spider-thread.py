338957e3dd4cf28614a764eab54b8bb8df3557cb
yzssbo <894903275@qq.com>
2020-07-30 18:17:12 +0800

    Update spider-thread.py

--- spider-thread.py
+++ spider-thread.py
@@ -26,7 +26,7 @@ class Mhxy(object):
         # 选择一个数据库
         self.db = self.client['admin']
         # 这里是认证登录所以需要进入admin进行登录, 不使用auth认证登录的mongo可以忽略
-        self.db.authenticate('yzs', 'yy961124')
+        self.db.authenticate('账号', '密码')
         # 选择一个集合
         self.col = self.client['test']['175_mhxy']
         self.info = 'return_url=; _ntes_nnid=510cab6547aa0e440af816dde796c00d,1576391392098; _ntes_nuid=510cab6547aa0e440af816dde796c00d; fingerprint=u8xbeja3vzkavszi; __session__=1; area_id=1; cbg_qrcode=dX-NS21tu_SXXA547UqK9iw1sQbqOyIq6AhO86u2; sid=yVKAKllBcWusoGzN3RaSMdi9XVKJHMDjkYVbIHT5; cur_servername=%25E7%258F%258D%25E5%25AE%259D%25E9%2598%2581; wallet_data=%7B%22is_locked%22%3A%20false%2C%20%22checking_balance%22%3A%200%2C%20%22balance%22%3A%200%2C%20%22free_balance%22%3A%200%7D; last_login_roleid=32312910; login_user_nickname=%25E5%2588%25AB%25E6%2589%2593%25E6%2584%259F%25E6%2583%2585%25E7%2589%258C; is_user_login=1; login_user_icon=203; login_user_roleid=32312910; login_user_level=30; login_user_urs=m18861816385@163.com; login_user_school=3; new_msg_num=13; offsale_num=0; unpaid_order_num=0; unpaid_order_price_total=0.00; last_login_role_serverid=358; recommend_typeids=1,2,3,4; recommend_url=https://xyq.cbg.163.com/cgi-bin/query.py?act=recommend_search&recommend_type=1; last_login_serverid=358; remind_offsale=1; alert_msg_flag=1'
